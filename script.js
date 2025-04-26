dc92f079a772464feede90efdb440ff6ad6aecc3
egorkondrasov <egorkondrasov@icloud.com>
2025-04-27 00:38:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("works-btn").addEventListener("click", function() {
+    const worksList = document.querySelector(".works-list");
+    worksList.style.display = worksList.style.display === "block" ? "none" : "block";
+});
